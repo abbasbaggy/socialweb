9981cd1f5ac7a4e50174b55f872115b93ff56156
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-03-28 06:41:44 +0100

    api attempt28

--- twitterGet.php
+++ twitterGet.php
@@ -43,16 +43,17 @@ foreach($string as $items)
     echo "Followers: ". $items['user']['followers_count']."<br />";
     echo "Friends: ". $items['user']['friends_count']."<br />";
     echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-}*
+}*/
 foreach($string as $items)
 {
     echo $items['id']."<br />";
-    echo "Name: ".$items['screen_name']."<br />";
+    echo "TWEET: ".$items['text']."<br />";
 
 
-}*/
+}
+/*
 echo "<pre>";
 print_r($string);
 echo "</pre>";
-
+*/
 ?>
