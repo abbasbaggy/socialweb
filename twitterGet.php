c13bc8b5f6cd8522cae747c22a8c07d0cd45b996
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-03-28 06:09:17 +0100

    api attempt26

--- twitterGet.php
+++ twitterGet.php
@@ -18,13 +18,14 @@ $settings = array(
 
 //$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
 //$url = "https://api.twitter.com/1.1/search/tweets.json";
-$url = "https://api.twitter.com/1.1/statusess/filter.json";
+$url = "https://stream.twitter.com/1.1/statusess/filter.json";
 $requestMethod = "GET";
 //if (isset($_GET['user']))  {$user = $_GET['user'];}
 //else {$user  = "iagdotme";}
 if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 20;}
 //$getfield = "?screen_name=$user&count=$count";
-$getfield = "?q=%23news&result_type=popular&count=$count";
+//$getfield = "?q=%23news&result_type=popular&count=$count";
+$getfield = "?track=#news";
 
 $twitter = new TwitterAPIExchange($settings);
 $string = json_decode($twitter->setGetfield($getfield)
