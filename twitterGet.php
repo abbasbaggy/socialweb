9daa89dd64d4fbce2d8d664753e8ed8f75d19c52
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-03-28 12:10:51 +0100

    api attempt31

--- twitterGet.php
+++ twitterGet.php
@@ -28,12 +28,15 @@ $requestMethod = "GET";
 $getfield = "?q=%23news&src=typd";
 
 $twitter = new TwitterAPIExchange($settings);
-$string = json_decode($twitter->setGetfield($getfield)
+echo $twitter->setGetfield($getfield)
+    ->buildOauth($url, $requestMethod)
+    ->performRequest();
+/*$string = json_decode($twitter->setGetfield($getfield)
     ->buildOauth($url, $requestMethod)
     ->performRequest(),$assoc = TRUE);
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p>
 <p><em>".$string[errors][0]["message"]."</em></p>";exit();}
-/*
+
 foreach($string as $items)
 {
     echo "Time and Date of Tweet: ". $items['created_at']."<br />";
@@ -52,9 +55,9 @@ foreach($string as $items)
 
 
 }
-*/
+*
 echo "<pre>";
 print_r($string);
 echo "</pre>";
-
+*/
 ?>
