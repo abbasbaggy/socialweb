d73cad47d22dda3604c1e41e2db07712078e076a
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-03-29 15:07:25 +0100

    api attempt1

--- twitterGet.php
+++ twitterGet.php
@@ -16,16 +16,16 @@ $settings = array(
     'consumer_secret' => "c87nNbExVMMMzO0gVALSPD9pTyz6IQZZKsP5p2B6VkikQwP6Lp"
 );
 
-//$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-$url = "https://api.twitter.com/1.1/search/tweets.json";
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+//$url = "https://api.twitter.com/1.1/search/tweets.json";
 
 $requestMethod = "GET";
-//if (isset($_GET['user']))  {$user = $_GET['user'];}
-//else {$user  = "iagdotme";}
-//if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 20;}
-//$getfield = "?screen_name=$user&count=$count";
+if (isset($_GET['user']))  {$user = $_GET['user'];}
+else {$user  = "iagdotme";}
+if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 20;}
+$getfield = "?screen_name=$user&count=$count";
 //$getfield = "?q=%23news&result_type=popular&count=$count";
-$getfield = "?q=%23news&src=typd";
+//$getfield = "?q=%23news&src=typd";
 
 $twitter = new TwitterAPIExchange($settings);
 
@@ -35,7 +35,7 @@ $string = json_decode($twitter->setGetfield($getfield)
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p>
 <p><em>".$string[errors][0]["message"]."</em></p>";exit();}
 
-/*
+
 foreach($string as $items)
 {
     echo "Time and Date of Tweet: ". $items['created_at']."<br />";
@@ -45,7 +45,7 @@ foreach($string as $items)
     echo "Followers: ". $items['user']['followers_count']."<br />";
     echo "Friends: ". $items['user']['friends_count']."<br />";
     echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-}*/
+}/*
 foreach($string as $items)
 {
     echo "Time and Date of Tweet: ".$items['created_at']."<br />";
