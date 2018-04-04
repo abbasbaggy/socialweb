31edaf172977fba0da4d39d185bd13b37d4957f0
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-04 09:46:38 +0100

    news api6

--- index.php
+++ index.php
@@ -79,7 +79,9 @@
         </div>
         <div class="col-sm-7 text-left">
             <h1>Welcome</h1>
-            <a> https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3 </a>
+            <script>
+            src= 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3'
+            </script>
             <?php include ('usenewsapi.php');
 
             ?>
