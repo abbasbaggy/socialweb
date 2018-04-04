4390a6c479b35ab34561ec29619c432bfc26379e
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-04 07:24:43 +0100

    news api3

--- index.php
+++ index.php
@@ -81,9 +81,9 @@
             <h1>Welcome</h1>
 
             <?php //include ('getnewsapi.php');
-            $response = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3";
+            //$response = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3";
             $resArr = array();
-            $resArr = json_decode($response,$ass= TRUE);
+            $resArr = json_decode("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3",$ass= TRUE);
             echo "<pre>";
             print_r($resArr);
             echo "</pre>";
