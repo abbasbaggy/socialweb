9aaf116c86f67fe21125d934e416e5f2f780dcaa
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-07 08:20:41 +0100

    bbcnews api6

--- bbcnewsapi.php
+++ bbcnewsapi.php
@@ -10,14 +10,14 @@
 $response= file_get_contents("https://newsapi.org/v2/everything?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3");
 $responsearray= json_decode($response,true);
 //die("{$responsearray}");
-/*
+
 echo "<pre>";
         print_r($responsearray);
         echo "</pre>";
-*/
 
 
 
+/*
 
 foreach ($responsearray as $item)
 
@@ -33,3 +33,4 @@ foreach ($responsearray as $item)
 
 }
 
+*?
