c5bac272d821bb8af00cbb7438f74e59913802d8
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-11 15:06:29 +0100

    microsoft api6

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
             <?php
            // include ('bbcnewsapi.php');
 
-
+            include ('MicrosoftNLP/SenMnlp.php')
             ?>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing  t.</p>
@@ -96,7 +96,7 @@
                 <p>Source Score</p>
             </div>
             <div class="well">
-                <p><?php include ('MicrosoftNLP/SenMnlp.php'); ?> </p>
+                <p><?php// include ('MicrosoftNLP/SenMnlp.php'); ?> </p>
             </div>
         </div>
     </div>
