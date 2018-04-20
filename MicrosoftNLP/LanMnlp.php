be07daba3e31e927131a4b3e034c743388c0fd1d
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-20 20:38:24 +0100

    microsoft api23

--- MicrosoftNLP/LanMnlp.php
+++ MicrosoftNLP/LanMnlp.php
@@ -62,16 +62,16 @@ print "Please wait a moment for the results to appear.";
 $result = DetectLanguage ($host, $path, $accessKey, $data);
 
 //echo json_encode (json_decode ($result), JSON_PRETTY_PRINT);
-//echo $result;
+echo $result;
 
 
-$lan = json_decode($result, true);
+//$lan = json_decode($result, true);
 
 /*
 echo "<pre>";
 print_r($lan);
 echo "</pre>";
-*/
+
 foreach ($lan as $lans){
     echo $lans['documents'][0]['detectedLanguages'][0]['name'] ."<br/>";
 }
