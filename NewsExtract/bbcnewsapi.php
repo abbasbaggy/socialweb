b5531d1c6bb5ed3d04d943e3c40ab830f8f90ca1
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-23 18:05:55 +0100

    news db4

--- NewsExtract/bbcnewsapi.php
+++ NewsExtract/bbcnewsapi.php
@@ -7,7 +7,7 @@
  */
 
 
-require ('../dbconnect.php');
+//require ('../dbconnect.php');
 $response = file_get_contents("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3");
 //$response= file_get_contents("https://newsapi.org/v2/everything?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3");
 $responsearray= json_decode($response,true);
