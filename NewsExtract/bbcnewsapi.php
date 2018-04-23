44d8567c9a7b6973255ba5898acc596cf2b331af
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-23 18:25:32 +0100

    news db5

--- NewsExtract/bbcnewsapi.php
+++ NewsExtract/bbcnewsapi.php
@@ -6,7 +6,7 @@
  * Time: 11:40
  */
 
-
+require_once("dbconnect.php");
 
 $response = file_get_contents("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3");
 //$response= file_get_contents("https://newsapi.org/v2/everything?sources=bbc-news&apiKey=5c167ce6600f424281d02fa7891d6ee3");
@@ -30,16 +30,16 @@ foreach ($responsearray as $item)
 
         $query = "INSERT INTO bbcnewstop(`Published`, `Title`, `Description`) VALUES
                (`".$row["publishedAt"]."`,`".$row["title"]."`,`".$row["description"]."`);";
-        require_once ("../dbconnect.php");
+
         mysqli_query($link, $query) or die(mysqli_error());
 
-    /*
+
         echo "Published At: " . $row['publishedAt'] . "<br />";
         //echo "Author :" . $row['author'] . "<br />";
         echo "Title :" . $row['title'] . "<br />";
         echo "Description :" . $row['description'] . "<br />";
         echo "URL :" . $row['url'] . "<br />";
-       */
+
     }
 
 
