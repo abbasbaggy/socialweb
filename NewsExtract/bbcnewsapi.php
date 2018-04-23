57724505156ad7714752f0b062eac012150e332f
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-23 18:49:08 +0100

    news db7

--- NewsExtract/bbcnewsapi.php
+++ NewsExtract/bbcnewsapi.php
@@ -31,7 +31,7 @@ foreach ($responsearray as $item)
         $query = "INSERT INTO bbcnewstop(`Published`, `Title`, `Description`) VALUES
                (`".$row["publishedAt"]."`,`".$row["title"]."`,`".$row["description"]."`);";
 
-        mysqli_query($link, $query) or die(mysqli_error($link));
+        mysqli_query($link, $query);
 
 
       /*  echo "Published At: " . $row['publishedAt'] . "<br />";
