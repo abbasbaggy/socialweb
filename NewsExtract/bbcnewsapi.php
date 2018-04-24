d24b77b22a0f9dc165d4724dc6799b56bcc9ac50
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-24 22:48:49 +0100

    news db19

--- NewsExtract/bbcnewsapi.php
+++ NewsExtract/bbcnewsapi.php
@@ -28,14 +28,17 @@ foreach ($responsearray as $item)
     foreach ($item as $row) {
         // use if statement to check date of last data b4 insering if(){
 
-       // $pub = mysqli_real_escape_string($link,$row['publishedAt']);
-        //$tit = mysqli_real_escape_string($link,$row['title']);
-        //$des = mysqli_real_escape_string($link,$row['description']);
+        $pub = mysqli_real_escape_string($con,$row['publishedAt']);
+        $tit = mysqli_real_escape_string($con,$row['title']);
+        $des = mysqli_real_escape_string($con,$row['description']);
+
+        $query = "INSERT INTO `bbcnewstop`(`‘Published’`, `‘Title’`, `‘Description’`) VALUES
+                  ('$pud','$tit','$des')";
 
        // $query = "INSERT INTO `bbcnewstop`(`‘Published’`, `‘Title’`, `‘Description’`) VALUES
           //     (\'".$row['publishedAt']."\','".$row['title']."','".$row["description"]."');";
 
-        $query = "INSERT INTO bbcnewstop(`‘Published’`, `‘Title’`, `‘Description’`) VALUES (\'2018-04-24T00:05:38Z\',\'Parents facing unfair child abuse claims over bruising\',\'Chloes son was taken away for a year, until his bruising was found to be down to a medical condition.\');";
+      //  $query = "INSERT INTO bbcnewstop(`‘Published’`, `‘Title’`, `‘Description’`) VALUES (\'2018-04-24T00:05:38Z\',\'Parents facing unfair child abuse claims over bruising\',\'Chloes son was taken away for a year, until his bruising was found to be down to a medical condition.\');";
 
         mysqli_query($con, $query);
 
