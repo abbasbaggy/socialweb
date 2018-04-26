916072b3482ae78360ecd3c52158dfc3ca118efa
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-26 18:11:12 +0100

    phrase 13

--- MicrosoftNLP/PhraMnlp.php
+++ MicrosoftNLP/PhraMnlp.php
@@ -73,10 +73,10 @@ echo "</pre>";
 //echo $pass['documents'][0]['keyPhrases'][0];
 $phrase = "";
 for($num = 0;$num <=10 ; $num++){
-    $phrase = $pass['documents'][0]['keyPhrases'][$num];
-    echo $phrase;
+    $phrase = $pass['documents'][0]['keyPhrases'][$num]. "<br /> ";
+   // echo $phrase;
 }
 
-
+echo $phrase;
 
 ?>
