c7623e231fe2402311f170572a19a6e2cf12bace
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-27 22:11:41 +0100

    phrase 15

--- MicrosoftNLP/PhraMnlp.php
+++ MicrosoftNLP/PhraMnlp.php
@@ -71,11 +71,11 @@ echo "</pre>";
 */
 
 //echo $pass['documents'][0]['keyPhrases'][0];
-$phrase = "";
+$phrase ;
 for($num = 0;$num <=10 ; $num++){
     $phrase = $pass['documents'][0]['keyPhrases'][$num]. "<br /> ";
-    echo $phrase;
+   // echo $phrase;
 }
 
-
+echo $phrase;
 ?>
