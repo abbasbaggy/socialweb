14a7c0139133bdedd797367622b9a17c3f386eaf
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-04-28 01:35:54 +0100

    phrase 4

--- MicrosoftNLP/PhraMnlp.php
+++ MicrosoftNLP/PhraMnlp.php
@@ -74,13 +74,13 @@ echo "</pre>";
 $phrase;
  $search = "";
 require ('dbconnect.php');
-for($num = 0;$num <=10 ; $num++){
-    $phrase += $pass['documents'][0]['keyPhrases'][$num]." ";
+    for($num = 0;$num <=10 ; $num++){
+    $phrase=+ $pass['documents'][0]['keyPhrases'][$num]." ";
     return $phrase;
 
     //$search = 'SELECT * FROM `bbcnewstop` WHERE (`Title` LIKE '%".$phrase."%')';
 
-}
+    }
     echo $phrase;
 //$search = 'SELECT * FROM `bbcnewstop` WHERE (`Title` LIKE '%".$phrase."%')';
 
