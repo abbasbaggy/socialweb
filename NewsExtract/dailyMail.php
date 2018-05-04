e8d28f5e9b9db4690d15c649dc50c0def9b0fdbc
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-05 00:17:01 +0100

    news api 5

--- NewsExtract/dailyMail.php
+++ NewsExtract/dailyMail.php
@@ -24,9 +24,9 @@ foreach ($responsearray['articles'] as $row)
 
 
         //$query = "INSERT INTO" for when i set db up
-        $pub = mysqli_real_escape_string($con,$row['publishedAt']);
-        $tit = mysqli_real_escape_string($con,$row['title']);
-        $des = mysqli_real_escape_string($con,$row['description']);
+     //   $pub = mysqli_real_escape_string($con,$row['publishedAt']);
+       // $tit = mysqli_real_escape_string($con,$row['title']);
+       // $des = mysqli_real_escape_string($con,$row['description']);
         //$inTime = mysqli_real_escape_string($con,$inTim);
 
         $query = "INSERT INTO `dailymail`(`Published`,`Title`,`Description`,`inTime`) VALUES
