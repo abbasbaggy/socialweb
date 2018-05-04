1570208201083464218d34fd9839b46c6ab9ea3d
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-04 15:21:20 +0100

    news job 5

--- NewsExtract/dailyMail.php
+++ NewsExtract/dailyMail.php
@@ -7,7 +7,37 @@
  */
 
 
-include('dbconnect.php');
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+$con = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$con) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+
+
+
+
+
+
 //$response=file_get_contents("https://newsapi.org/v2/top-headlines?sources=daily-mail&apiKey=5c167ce6600f424281d02fa7891d6ee3");
 $response= file_get_contents("https://newsapi.org/v2/everything?sources=daily-mail&apiKey=5c167ce6600f424281d02fa7891d6ee3");
 $responsearray= json_decode($response,true);
@@ -31,14 +61,15 @@ foreach ($responsearray as $item)
                   ('$pub',' $tit ',' $des' ,'$inTime');";
         mysqli_query($con, $query);
 
-        echo "Published At: " . $row['publishedAt'] . "<br />";
+      /*  echo "Published At: " . $row['publishedAt'] . "<br />";
         // echo "Author :" . $row['author'] . "<br />";
         echo "Title :" . $row['title'] . "<br />";
         echo "Description :" . $row['description'] . "<br />";
         echo "URL :" . $row['url'] . "<br />";
         echo "time " .$inTime. "<br/>";
+      */
     }
 
 }
-echo "Errors".   mysqli_error($con);
+//echo "Errors".   mysqli_error($con);
 
