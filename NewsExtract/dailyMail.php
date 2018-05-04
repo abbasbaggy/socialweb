4aaed4cd10d0bc3683881c22383da1ec83d60024
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-05 00:25:36 +0100

    news api 5

--- NewsExtract/dailyMail.php
+++ NewsExtract/dailyMail.php
@@ -43,5 +43,10 @@ foreach ($responsearray['articles'] as $row)
 
 
 }
-echo "Errors".   mysqli_error($con);
+if (!$con) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
 
