a30ef1d796feb60c085836762a74336b4097352a
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-04 16:00:46 +0100

    news job 6

--- index.php
+++ index.php
@@ -98,25 +98,6 @@
 
             ?>
 
-            <script>
-
-
-
-
-                }
-
-
-                / When the user scrolls down 20px from the top of the document, slide down the navbar
-                window.onscroll = function() {scrollFunction()};
-
-                function scrollFunction() {
-                    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                        document.getElementById("navbar").style.top = "0";
-                    } else {
-                        document.getElementById("navbar").style.top = "-50px";
-                    }
-                }
-            </script>
 
 
 
@@ -137,7 +118,7 @@
 
 
 
-    /
+
 
 
 <footer class="container-fluid text-center">
