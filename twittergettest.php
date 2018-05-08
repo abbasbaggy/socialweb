f106dd0efa22b50caf0f3276f9429f4d3d88d347
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-08 12:57:38 +0100

    twitter try 4

--- twittergettest.php
+++ twittergettest.php
@@ -43,48 +43,8 @@ $string = $twitter->setGetfield($getfield)
     ->performRequest();
 //if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p>
 //<p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+echo "<pre>";
+print_r($string);
+echo "</pre>";
 
 ?>
-<script>
-$(document).ready(function() {
-loadLatestTweet();
-});
-
-//Twitter Parsers
-String.prototype.parseURL = function() {
-return this.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&~\?\/.=]+/g, function(url) {
-return url.link(url);
-});
-};
-String.prototype.parseUsername = function() {
-return this.replace(/[@]+[A-Za-z0-9-_]+/g, function(u) {
-var username = u.replace("@","")
-return u.link("http://twitter.com/"+username);
-});
-};
-String.prototype.parseHashtag = function() {
-return this.replace(/[#]+[A-Za-z0-9-_]+/g, function(t) {
-var tag = t.replace("#","%23")
-return t.link("http://search.twitter.com/search?q="+tag);
-});
-};
-function parseDate(str) {
-var v=str.split(' ');
-return new Date(Date.parse(v[1]+" "+v[2]+", "+v[5]+" "+v[3]+" UTC"));
-}
-
-function loadLatestTweet(){
-var numTweets = 1;
-var _url = <?php $string ?>;
-$.getJSON(_url,function(data){
-for(var i = 0; i< data.length; i++){
-var tweet = data[i].text;
-var created = parseDate(data[i].created_at);
-var createdDate = created.getDate()+'-'+(created.getMonth()+1)+'-'+created.getFullYear()+' at '+created.getHours()+':'+created.getMinutes();
-tweet = tweet.parseURL().parseUsername().parseHashtag();
-tweet += '<div class="tweeter-info"><div class="uppercase bold"><a href="https://twitter.com/#!/CypressNorth" target="_blank" class="black">@CypressNorth</a></div><div class="right"><a href="https://twitter.com/#!/CypressNorth/status/'+data[i].id_str+'">'+createdDate+'</a></div></div>'
-$("#twitter-feed").append('<p>'+tweet+'</p>');
-}
-});
-}
-</script>
