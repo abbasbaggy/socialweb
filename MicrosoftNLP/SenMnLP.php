05ce506f87a5b6a0cb672feac53570fdea8ff618
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-08 02:11:46 +0100

    phrase sql 14

--- MicrosoftNLP/SenMnLP.php
+++ MicrosoftNLP/SenMnLP.php
@@ -63,25 +63,25 @@ $senti = $sen['documents'][0]['score'];
 echo $senti ."<br/>";
 
 switch ($senti){
-    case (0.00 < 0.20):
+    case ($senti <= 0.20):
         echo "extremely Negative .\"<br/>\"";
         break;
-    case (0.21 < 0.40):
+    case ($senti <= 0.40):
         echo "very Negative .\"<br/>\"";
         break;
-    case (0.41 < 0.49):
+    case ($senti <= 0.49):
         echo "a little negative .\"<br/>\"";
         break;
-    case (0.5):
+    case ($sent = 0.5):
         echo "Neutral .\"<br/>\"";
         break;
-    case (0.51 < 0.59):
+    case ($senti <= 0.59):
         echo "a little positive .\"<br/>\"";
         break;
-    case (0.60 < 0.80):
+    case ($senti <= 0.80):
         echo "very positive .\"<br/>\"";
         break;
-    case (0.81 < 1.0):
+    case ($senti <= 1.0):
         echo "extremely positive .\"<br/>\"";
         break;
 }
