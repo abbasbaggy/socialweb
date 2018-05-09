ea03ac5761d30f2a6025775c9c6096419eef2fc5
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-09 12:50:49 +0100

    select tweet7

--- twittergettest.php
+++ twittergettest.php
@@ -44,6 +44,7 @@ $string = json_decode($twitter->setGetfield($getfield)
 if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p>
 <p><em>".$string[errors][0]["message"]."</em></p>";exit();}
 
+$twits =json_encode (json_decode ($string), JSON_PRETTY_PRINT);
 /*
 foreach($string as $items)
 {
@@ -72,80 +73,13 @@ foreach($string['statuses'] as $item){
     echo "Screen name :" . $item['user']['screen_name'] . "<br />";
     echo "favourites count :" . $item['user']['favourites_count'] . "<br />";
     ?>
+    <span id= "<?php echo $i?>" style="background-color: azure" > <?php   echo "TWEETs:  " . $item['text']. "<br/>" ?> </span>
+    <button id = "<?php echo $in?>">Select tweet</button>
 
-    <form>
-        <div class="input-group">
-            <input type="text" class="form-control"
-                   value="<?php   echo "TWEETs:  " . $item['text'] ?>" placeholder="Some path" id="copy-input">
-            <span class="input-group-btn">
-      <button class="btn btn-default" type="button" id="copy-button"
-              data-toggle="tooltip" data-placement="button"
-              title="Copy to Clipboard">
-        Copy
-      </button>
-    </span>
-        </div>
-    </form>
+
 
     <script>
-
-        $('#copy-button').tooltip();
-
-        $('#copy-button').bind('click', function() {
-            var input = document.querySelector('#copy-input');
-            input.setSelectionRange(0, input.value.length + 1);
-            try {
-                var success = document.execCommand('copy');
-                if (success) {
-                    // Change tooltip message to "Copied!"
-                } else {
-                    // Handle error. Perhaps change tooltip message to tell user to use Ctrl-c
-                    // instead.
-                }
-            } catch (err) {
-                // Handle error. Perhaps change tooltip message to tell user to use Ctrl-c
-                // instead.
-            }
-        });
-
-        $('#copy-button').bind('copied', function(event, message) {
-            $(this).attr('title', message)
-                .tooltip('fixTitle')
-                .tooltip('show')
-                .attr('title', "Copy to Clipboard")
-                .tooltip('fixTitle');
-        });
-        $(document).ready(function() {
-            // Initialize the tooltip.
-            $('#copy-button').tooltip();
-
-            // When the copy button is clicked, select the value of the text box, attempt
-            // to execute the copy command, and trigger event to update tooltip message
-            // to indicate whether the text was successfully copied.
-            $('#copy-button').bind('click', function() {
-                var input = document.querySelector('#copy-input');
-                input.setSelectionRange(0, input.value.length + 1);
-                try {
-                    var success = document.execCommand('copy');
-                    if (success) {
-                        $('#copy-button').trigger('copied', ['Copied!']);
-                    } else {
-                        $('#copy-button').trigger('copied', ['Copy with Ctrl-c']);
-                    }
-                } catch (err) {
-                    $('#copy-button').trigger('copied', ['Copy with Ctrl-c']);
-                }
-            });
-
-            // Handler for updating the tooltip message.
-            $('#copy-button').bind('copied', function(event, message) {
-                $(this).attr('title', message)
-                    .tooltip('fixTitle')
-                    .tooltip('show')
-                    .attr('title', "Copy to Clipboard")
-                    .tooltip('fixTitle');
-            });
-        });
+    Var newstri = <?php echo $twits ?>
 
     </script>
 
