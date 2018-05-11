3ac20417a0eaef4fe4316f5c94d8cacb8b02c748
Abbas <abbas_yusuff2002@yahoo.co.uk>
2018-05-11 16:36:15 +0100

    19

--- MicrosoftNLP/PhraMnlp.php
+++ MicrosoftNLP/PhraMnlp.php
@@ -75,7 +75,7 @@ require ('../MicrosoftNLP/dbconnect.php');
 
 foreach ($pass['documents'][0]['keyPhrases'] as $phrase){
     $TStrps = mysqli_real_escape_string($con,$phrase);
-    $datasen = "SELECT * FROM `bbcnewstop` `LIMIT 2` WHERE (`Title` LIKE '%".$TStrps."%') ";
+    $datasen = "SELECT * FROM `bbcnewstop`  WHERE (`Title` LIKE '%".$TStrps."%') `LIMIT 2` ";
     $query = mysqli_query($con,$datasen);
 
     if(mysqli_num_rows($query)> 0){
